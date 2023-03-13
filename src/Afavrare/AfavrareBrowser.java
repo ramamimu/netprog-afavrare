b26ae1db2173f77bffe0506cdc19468f4ca3574f
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2023-03-13 21:21:38 +0700

    Ok, now bisa socket and set get and read response yang good

--- src/Afavrare/AfavrareBrowser.java
+++ src/Afavrare/AfavrareBrowser.java
@@ -3,16 +3,17 @@ package Afavrare;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AfavrareBrowser {
     // Attribute nya
     public Socket socket;
+    public String header;
+    public String body;
+    public int statusCode;
 
     // Constructor nya
     public AfavrareBrowser(){
@@ -29,71 +30,31 @@ public class AfavrareBrowser {
         String host = scanner.nextLine();
         int port = 80; // default HTTP
 
-
         // connect time after got user input
         try{
             this.socket = new Socket(host, port);
-            this.socket.setSoTimeout(5000); // timeout after 5 seconds
+            this.socket.setSoTimeout(3000); // timeout after 3 seconds
 
             // chaining socket's input stream to buffered input stream so that we are able to read more than 1 byte
-            BufferedInputStream bis = new BufferedInputStream(this.socket.getInputStream());
             BufferedOutputStream bos = new BufferedOutputStream(this.socket.getOutputStream());
+            BufferedInputStream bis = new BufferedInputStream(this.socket.getInputStream());
 
-            String http_request = this.make_http_request_msg("GET", "/index.html", host);
-            System.out.print(http_request);
+            String http_request = this.make_http_request_msg("GET", "/", host);
 
             bos.write(http_request.getBytes(StandardCharsets.UTF_8), 0, http_request.length());
             bos.flush();
-            System.out.print("Done flushing\n");
 
-            String response = "";
-            int counter = 0;
-            boolean isDone = false;
-            int byteRead = 0;
-            while(true) {
-                byte[] input = new byte[512];
-                try{
-                    byteRead = bis.read(input);
-                }
-                catch (SocketTimeoutException ex){
-                    break;
-                }
-//                System.out.print("Done reading\n");
-                response += new String(input);
+            // read the response
+            this.header = this.read_header_response(bis);
 
-                String bufarray[] = response.split("\r\n");
-//                System.out.printf("Done splitting, got over %d string after split\n", bufarray.length);
+            // ok, separate the body that accidentally got in header
+            this.extractBody();
 
-                for(int i = 0;i<bufarray.length;i++){
-//                    System.out.printf("We got split #%d: %s\n", i+1,bufarray[i]);
-                }
+            // ok read the remaining body
+            int contentLength = this.getContentLength();
+            this.body += this.read_body_response(bis, contentLength);
 
-                for (int i =0; i<bufarray.length-1;i++) {
-                    if(counter == 0) {
-                        System.out.println(bufarray[i]);
-                    }
-                    else if (bufarray[i].length()>0) {
-                        String[] header = bufarray[i].split(":");
-                        System.out.println("=======\n" + header[0] + "\n" + header[1].trim());
-
-                    }
-
-                    else {
-                        isDone = true;
-                        break;
-                    }
-
-                    response = bufarray[bufarray.length-1];
-                    if (isDone) {
-                        break;
-                    }
-
-                    counter++;
-                }
-                System.out.print("For loop parsing is done\n");
-            }
-            // end
-            this.close();
+            System.out.printf("%s\n", this.body);
 
         }catch (IOException ex){
             System.err.print(ex);
@@ -101,17 +62,92 @@ public class AfavrareBrowser {
 
     }
 
-    public String make_http_request_msg(String method, String pathToResource, String host){
+    private String make_http_request_msg(String method, String pathToResource, String host){
         String msg = "";
         msg += method + " " + pathToResource + " HTTP/1.1\r\n";
-//        msg += "Host: " + this.socket.getInetAddress() + "\n";
-        msg += "Host: " + host + "\r\n";
-        msg += "Connection: keep-alive\r\n";
-        msg += "Accept-Language: en-US\r\n";
-        msg += "Accept-Encoding: gzip, deflate\r\n";
-        msg += "Accept: text/html\r\n\r\n";
+        msg += "Host: " + host + "\r\n\r\n";
         return msg;
 
+    }
+
+    private String read_header_response(BufferedInputStream bis){
+        String response = "";
+        byte[] bufferInput = new byte[512];
+        int bytesRead = 0;
+        boolean isReading=true;
+        while(isReading){
+            try{
+                // try to read 512 byte
+                bytesRead = bis.read(bufferInput);
+                if(bytesRead < 0){
+                    // -1 mean no byte to read
+                    isReading=false;
+                }else{
+                    // ok not -1, some byte readed, append to response
+                    response += new String(bufferInput, 0, bytesRead);
+                    // stop if ada double new line (end of header)
+                    isReading = isReading && !response.contains("\r\n\r\n");
+                }
+
+            }
+            catch (IOException ex){
+                break;
+            }
+
+        }
+
+        return response;
+    }
+
+    private String read_body_response(BufferedInputStream bis, int contentLength){
+        int remainingLength = contentLength - this.body.length();
+        String response = "";
+        byte[] bufferInput = new byte[512];
+        int bytesRead = 0;
+        while(response.length() < remainingLength){
+            try{
+                // try to read 512 byte
+                bytesRead = bis.read(bufferInput);
+                if(bytesRead < 0){
+                    break;
+                }else{
+                    // ok not -1, some byte readed, append to response
+                    response += new String(bufferInput, 0, bytesRead);
+                }
+
+            }
+            catch (IOException ex){
+                break;
+            }
+
+        }
+        return response;
+    }
+
+    private int getContentLength(){
+
+        int indexContentLength = this.header.indexOf("content-length:");
+        int indexNewLine = this.header.indexOf("\r\n", indexContentLength);
+        int contentLength=0;
+        try{
+            String integerPart = this.header.substring(indexContentLength+16, indexNewLine);
+            contentLength = Integer.parseInt(integerPart);
+        }
+        catch (StringIndexOutOfBoundsException ex){
+            //ignore
+        }
+
+        return contentLength;
+    }
+
+
+
+    private void extractBody(){
+        int indexBodyStart = this.header.indexOf("\r\n\r\n");
+        this.body = this.header.substring(indexBodyStart+2);//+2 to eliminate 2 newline
+
+        // keep newline in header
+        this.header = this.header.substring(0, indexBodyStart+2).toLowerCase();
     }
 
     // to close the browser and all the connection
